6ae33432388f7f75a7ab4a3c93dfa26e8fce203c
zoey-t <101599447+zoey-t@users.noreply.github.com>
2023-02-16 18:47:34 -0800

    more info

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,9 @@ import {process_finding_issue} from './finding'
 async function run(): Promise<void> {
 	try {
 		const configs = await getInputs()
-		await process_finding_issue(configs)
+		const finding_md = await process_finding_issue(configs)
+		core.info(`file name ${finding_md.fileName}`)
+		core.debug(`${finding_md.md}`)
 	} catch (error) {
 		if (error instanceof Error) core.setFailed(error.message)
 	}
